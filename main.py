d0ef74503751737813838858f625fb0c2b6970fd
LexiusTW <storchaklexius@mail.ru>
2024-12-05 17:08:12 +0500

    Создан класс с сущностью заявки, наследуемый от BaseModel, импортированы BaseModel и Literal

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
+from datetime import date
+from typing import Literal
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Orders(BaseModel):
+    id: int
+    dateStart: date
+    device: str
+    problemType: str
+    description: str
+    client: str
+    status: Literal["в ожидании", "в работе", "выполнено"] = "в ожидании"
